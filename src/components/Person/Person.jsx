0de582d31ddf90686753c8261f6e80c40d08fa24
vkrasnovyd <v.krasnovyd@proton.me>
2025-03-19 17:55:26 +0100

    Refactor: ternary operator, destructuring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,12 @@
-export const Person = ({ person }) => (
+export const Person = ({ name, age, sex, isMarried, partnerName }) => (
   <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    {age && <p className="Person__age">{`I am ${age}`}</p>}
 
-    {person.isMarried ? (
-      <p className="Person__partner">{`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}</p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        : 'I am not married'}
+    </p>
   </section>
 );
